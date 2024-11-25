0465920ecd6853f2842899f6f3a43cd5228fff17
David Haasler <dhaasler@users.noreply.github.com>
2024-11-25 10:15:04 +0100

    Add documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+project = 'madcubapy'
+copyright = '2024, David Haasler García'
+author = 'David Haasler García'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google/NumPy style docstrings
+    'sphinx.ext.viewcode',  # To link source code
+    'sphinx.ext.githubpages'  # Optional: For GitHub Pages
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/madcubapy.utils.rst
@@ -0,0 +1,27 @@
+madcubapy.utils package
+=======================
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+Submodules
+----------
+
+madcubapy.utils.spectral module
+-------------------------------
+
+.. automodule:: madcubapy.utils.spectral
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: madcubapy.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
