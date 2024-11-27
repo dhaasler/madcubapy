1c95360b7889bc255cb56a13d6db937730f1767d
David Haasler <dhaasler@users.noreply.github.com>
2024-11-27 09:52:47 +0100

    Change astropy 7.0.0 to 7.0 in requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/dhaasler/madcubapy',
     packages=find_packages(),
     install_requires=[
-        "astropy>=7.0.0",
+        "astropy>=7.0",
         "matplotlib>=3.9.0",
         "numpy>=1.26.0"
     ],
