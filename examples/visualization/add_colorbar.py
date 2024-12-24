6e8c56783efdd92641b4c4ffe797c0a056b4228d
David Haasler <dhaasler@users.noreply.github.com>
2024-12-24 16:12:08 +0100

    Change add_colorbar to append_colorbar

--- examples/visualization/add_colorbar.py
+++ examples/visualization/add_colorbar.py
@@ -1,9 +1,8 @@
 """
-Plotting MadcubaMap
-###################
+Add colorbar to a MadcubaMap plot
+#################################
 
-Example showing how to plot a fits map file with MadcubaMap using the
-``add_wcs_axes`` function.
+Example showing how to quickly add a colorbar to a MadcubaMap plot.
 
 .. note::
     Due to how MADCUBA saves some fits header cards, several astropy warnings
@@ -13,14 +12,15 @@ Example showing how to plot a fits map file with MadcubaMap using the
 """
 
 from madcubapy.io import MadcubaMap
+from madcubapy.visualization import append_colorbar
 from madcubapy.visualization import add_wcs_axes
 import matplotlib.pyplot as plt
 
 # Read fits file
 madcuba_map = MadcubaMap.read("../data/IRAS16293_SO_2-1_moment0_madcuba.fits")
 
-# Create empty figure
-fig = plt.figure(figsize=(6,6))
-# Add as many WCS axes objects as desired. We can pass kwargs to imshow()
+fig = plt.figure(figsize=(6,5))
 ax, img = add_wcs_axes(fig, 1, 1, 1, fitsmap=madcuba_map, vmin=0, vmax=100)
+# Add the colorbar. We can pass kwargs to ax.colorbar() function of matplotlib
+append_colorbar(ax, label='Custom label')
 plt.show()
