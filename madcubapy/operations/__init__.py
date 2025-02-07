5132e1847851741168a236100f97a2e3700cc9f5
David Haasler <dhaasler@users.noreply.github.com>
2025-02-07 13:03:17 +0100

    Add docstrings to subpackage init files

--- madcubapy/operations/__init__.py
+++ madcubapy/operations/__init__.py
@@ -1,3 +1,8 @@
 # madcubapy/operations/__init__.py
 
+"""
+The `madcubapy.operations` package provides utility functions to help the user
+perform operations on data of FITS and .spec files.
+"""
+
 from .maps import *
--- madcubapy/visualization/__init__.py
+++ madcubapy/visualization/__init__.py
@@ -1,4 +1,10 @@
 # madcubapy/visualization/__init__.py
 
+"""
+The `madcubapy.visualization` package provides functions to help the user
+visualize astronomical data, with a special emphasis on products exported by
+MADCUBA.
+"""
+
 from .wcsaxes_helpers import *
 from .quick_plotters import *
