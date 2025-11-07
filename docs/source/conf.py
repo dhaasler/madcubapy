10882e08dde464059a0640c10777e58914ff5057
David Haasler <dhaasler@users.noreply.github.com>
2025-11-07 10:38:50 +0100

    Bump version to 0.8.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'madcubapy'
 copyright = '2025, David Haasler García'
 author = 'David Haasler García'
-release = '0.8.2'
+release = '0.8.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
